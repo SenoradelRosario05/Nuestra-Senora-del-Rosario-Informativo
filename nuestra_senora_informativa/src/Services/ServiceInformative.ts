4b72a9ab77814e308dd4a95b39725ee7ad7ce208
FabiolaC1 <fabicarrera1904@gmail.com>
2024-08-27 02:11:01 -0600

    chore/AboutUs

--- nuestra_senora_informativa/src/Services/ServiceInformative.ts
+++ nuestra_senora_informativa/src/Services/ServiceInformative.ts
@@ -1,15 +1,42 @@
 
 
 
+import axios from 'axios';
 
 
 
-import React from 'react'
+const URLBase = 'https://localhost:7066/api/AboutUsSection';
 
-const ServiceInformative = () => {
-  return (
-    <div>ServiceInformative</div>
-  )
+
+export const getAboutUsSection = async (id: number) => {
+    const response = await axios.get(`${URLBase}/subtitle_About_Us/${id}`);
+    return response.data;
 }
 
-export default ServiceInformative
+
+export const getMissionTitle = async () => {  
+const response = await axios.get(`${URLBase}/missionTitle_About_Us`);
+return response.data;
+}
+
+
+export const getMissionDescription = async () => {
+    const response = await axios.get(`${URLBase}/missionDescription_About_Us`);
+    return response.data;
+}
+
+
+
+export const getVisionTitle = async () => {
+  const response = await axios.get(`${URLBase}/visionTitle_About_Us`);
+  return response.data 
+
+}
+
+
+export const getVisionDescription = async () => {
+  const response = await axios.get(`${URLBase}/visionDescription_About_Us`);
+  return response.data;
+}
+
+ 
