37950f45501b492eea7df15500f9c8b21199c0c7
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-09-04 09:43:30 -0600

    feat/ update branch and fix the services

--- nuestra_senora_informativa/src/Pages/MainPage/Components/ServicesSection.tsx
+++ nuestra_senora_informativa/src/Pages/MainPage/Components/ServicesSection.tsx
@@ -4,11 +4,11 @@ import useTitles from "../../../Hooks/useTitles";
 import useServiceSection from "../Hooks/useServices";
 
 const ServiceSection = () => {
-  const { data: title } = useTitles(2); 
-  const { data: serviceData } = useServiceSection(); 
-  const [isFlipped, setIsFlipped] = useState<{ [key: number]: boolean }>({}); 
+  const { data: title } = useTitles(2);
+  const { data: serviceData } = useServiceSection();
+  const [isFlipped, setIsFlipped] = useState<{ [key: number]: boolean }>({});
 
-  const containerRef = useRef<HTMLDivElement>(null); 
+  const containerRef = useRef<HTMLDivElement>(null);
 
   // Función para manejar el flip de la tarjeta
   const handleFlip = (index: number) => {
@@ -23,7 +23,7 @@ const ServiceSection = () => {
     if (containerRef.current) {
       containerRef.current.scrollBy({
         left: -300,
-        behavior: 'smooth', 
+        behavior: "smooth",
       });
     }
   };
@@ -33,36 +33,46 @@ const ServiceSection = () => {
     if (containerRef.current) {
       containerRef.current.scrollBy({
         left: 300,
-        behavior: 'smooth', 
+        behavior: "smooth",
       });
     }
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center  px-2 sm:px-6 lg:px-8">
-     
-      <div className="w-[334px] text-[#0d313f] text-[32px] font-medium font-'Poppins' text-center mb-12">
-        <p>{title?.title_Text_Section}</p>
-        <br />
-        <p className="text-[#0d313f] text-2xl font-normal font-'Poppins'">
-          {title?.description_Section || "Brindamos distintos servicios de los cuales destacamos"}
-        </p>
-      </div>
-        
+    <div className="min-h-screen flex flex-col items-center px-2 sm:px-6 lg:px-8">
+<div className="w-full max-w-4xl text-[#0d313f] text-[32px] font-medium text-center mb-12">
+  <p>{title?.title_Text_Section}</p>
+  <br />
+  <p className="text-[#0d313f] text-2xl font-normal">
+    {title?.description_Section ||
+      "Brindamos distintos servicios de los cuales destacamos"}
+  </p>
+</div>
+
       <div className="relative w-full flex items-center">
-        
         <button
           onClick={scrollLeft}
           className="absolute left-4 z-10 text-gray-400 hover:text-gray-700 transition-colors duration-200 p-2"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M15 19l-7-7 7-7"
+            />
           </svg>
         </button>
 
         <div
           ref={containerRef}
-          className="flex space-x-8 max-w-full px-16 sm:px-16 overflow-x-scroll scrollbar-hide scroll-smooth" // Añadido 'scroll-smooth' para desplazamiento suave
+          className="flex space-x-8 max-w-full px-16 overflow-x-scroll scrollbar-hide scroll-smooth"
         >
           {serviceData?.map((service: any, index: number) => (
             <ReactCardFlip
@@ -70,37 +80,36 @@ const ServiceSection = () => {
               isFlipped={isFlipped[index]}
               flipDirection="horizontal"
             >
-             
+              {/* FRONT SIDE */}
               <div
-                className="w-[278px] h-[327px] relative bg-[#d9d9d9] rounded-[15px] shadow cursor-pointer"
+                className="w-[278px] h-[327px] relative bg-[#d9d9d9] rounded-[15px] shadow cursor-pointer flex flex-col items-center"
                 onClick={() => handleFlip(index)}
               >
-                <div className="left-[89px] top-[7px] absolute text-[#0d313f] text-xl font-normal font-'Poppins'">
+                <div className="mt-4 text-[#0d313f] text-xl font-medium">
                   {service.title_Card_SV}
                 </div>
                 <img
-                  className="w-[200px] h-[133.35px] left-[44px] top-[52px] absolute rounded-[10px]"
+                  className="w-[200px] h-[133.35px] my-4 rounded-[10px]"
                   src={service.image_Card_SV_Url}
                   alt={service.title_Card_SV}
                 />
-                <div className="w-[201px] left-[44px] top-[203px] absolute text-center text-[#0d313f] text-sm font-normal font-'Poppins'">
-                  {service.description_Card_SV.split('.')[0]}.
+                <div className="w-[201px] text-center text-[#0d313f] text-sm font-normal">
+                  {service.description_Card_SV.split(".")[0]}.
                 </div>
-
-                <div className="absolute inset-x-0 bottom-4 flex justify-center">
-                <button className="bg-[#dab87d] text-[#0d313f] text-[12px] sm:text-[14px] font-bold font-'Poppins' py-1 sm:py-2 px-4 sm:px-6 rounded-[10px] sm:rounded-[15px] shadow transition-all duration-300 hover:bg-[#c7a46d]">
-                 {service.buttonText || "Ver más"}
-                </button>
+                <div className="mt-auto mb-4">
+                  <button className="bg-[#dab87d] text-[#0d313f] text-[12px] sm:text-[14px] font-bold py-2 px-6 rounded-[10px] sm:rounded-[15px] shadow transition-all duration-300 hover:bg-[#c7a46d]">
+                    {service.buttonText || "Ver más"}
+                  </button>
                 </div>
               </div>
 
-              {/* Back Side */}
+              {/* BACK SIDE */}
               <div
                 className="w-[278px] h-[327px] relative bg-[#0d313f] text-white rounded-[15px] shadow flex flex-col justify-center items-center p-4 cursor-pointer"
-                onClick={() => handleFlip(index)} 
+                onClick={() => handleFlip(index)}
               >
                 <p className="text-sm mb-6 text-center">
-                  {service.description_Card_SV} 
+                  {service.description_Card_SV}
                 </p>
               </div>
             </ReactCardFlip>
@@ -111,8 +120,19 @@ const ServiceSection = () => {
           onClick={scrollRight}
           className="absolute right-4 z-10 text-gray-400 hover:text-gray-700 transition-colors duration-200 p-2"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 5l7 7-7 7"
+            />
           </svg>
         </button>
       </div>
