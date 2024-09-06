e4aeab991fdffff71d8423b6c640d85b8cbca9d7
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-09-06 09:25:33 -0600

    refactor/ arreglando algunos bugs

--- nuestra_senora_informativa/src/App.tsx
+++ nuestra_senora_informativa/src/App.tsx
@@ -4,18 +4,19 @@ import useRoutesConfig from "./Routes/routesConfig"
 import { Suspense } from "react"
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import LoadingSpinner from "./Components/LoadingSpinner";
 
 function App() {
   const { isLoading, routes } = useRoutesConfig();
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <LoadingSpinner />;
   }
 
   return (
     <Router>
       <Header />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<LoadingSpinner />}>
         <Routes>
           {routes.map((route) => (
             <Route key={route.path} path={route.path} element={route.element} />
--- nuestra_senora_informativa/src/Pages/MainPage/Components/ServicesSection.tsx
+++ nuestra_senora_informativa/src/Pages/MainPage/Components/ServicesSection.tsx
@@ -7,7 +7,6 @@ const ServiceSection = () => {
   const { data: title } = useTitles(2);
   const { data: serviceData } = useServiceSection();
   const [isFlipped, setIsFlipped] = useState<{ [key: number]: boolean }>({});
-
   const containerRef = useRef<HTMLDivElement>(null);
 
   // Función para manejar el flip de la tarjeta
@@ -39,17 +38,21 @@ const ServiceSection = () => {
   };
 
   return (
-    <div id="servicios" className="min-h-screen flex flex-col items-center px-2 sm:px-6 lg:px-8">
-<div className="w-full max-w-4xl text-[#0d313f] text-[32px] font-medium text-center mb-12">
-  <p>{title?.title_Text_Section}</p>
-  <br />
-  <p className="text-[#0d313f] text-2xl font-normal">
-    {title?.description_Section ||
-      "Brindamos distintos servicios de los cuales destacamos"}
-  </p>
-</div>
+    <div
+      id="servicios"
+      className="min-h-screen flex flex-col items-center px-2 sm:px-6 lg:px-8"
+    >
+      <div className="w-full max-w-4xl text-[#0d313f] text-[32px] font-medium text-center mb-12">
+        <p>{title?.title_Text_Section}</p>
+        <br />
+        <p className="text-[#0d313f] text-2xl font-normal">
+          {title?.description_Section ||
+            "Brindamos distintos servicios de los cuales destacamos"}
+        </p>
+      </div>
 
-      <div className="relative w-full flex items-center">
+      <div className="relative w-full flex items-center overflow-hidden">
+        {/* Botón de la izquierda */}
         <button
           onClick={scrollLeft}
           className="absolute left-4 z-10 text-gray-400 hover:text-gray-700 transition-colors duration-200 p-2"
@@ -70,6 +73,7 @@ const ServiceSection = () => {
           </svg>
         </button>
 
+        {/* Contenedor que permite el scroll con las flechas */}
         <div
           ref={containerRef}
           className="flex space-x-8 max-w-full px-16 overflow-x-scroll scrollbar-hide scroll-smooth"
@@ -116,6 +120,7 @@ const ServiceSection = () => {
           ))}
         </div>
 
+        {/* Botón de la derecha */}
         <button
           onClick={scrollRight}
           className="absolute right-4 z-10 text-gray-400 hover:text-gray-700 transition-colors duration-200 p-2"
