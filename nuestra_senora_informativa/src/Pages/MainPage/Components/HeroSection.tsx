3fe94db5cc21b8c4534e1cc2b47047384516a781
TosTar26 <ozsanchez2004@gmail.com>
2024-11-16 09:11:36 -0600

    refactor: refactoring the images

--- nuestra_senora_informativa/src/Pages/MainPage/Components/HeroSection.tsx
+++ nuestra_senora_informativa/src/Pages/MainPage/Components/HeroSection.tsx
@@ -1,43 +1,56 @@
-import { useQuery } from 'react-query';
-import { getHeroSection, getTitles } from '../../../Services/ServiceInformative';
-import useNavbarItemsId from '../../../Hooks/useNavbarItemsId';
-import useHandleScroll from '../../../Hooks/useHandleScroll';
+import { useQuery } from "react-query";
+import {
+  getHeroSection,
+  getTitles,
+} from "../../../Services/ServiceInformative";
+import useNavbarItemsId from "../../../Hooks/useNavbarItemsId";
+import useHandleScroll from "../../../Hooks/useHandleScroll";
 
 const HeroSection = () => {
-  const { data: TitleSection } = useQuery(['title', 1], () => getTitles(1));
-  const { data: HeroSection } = useQuery('hero', getHeroSection);
+  const { data: TitleSection, isLoading: isTitleLoading } = useQuery(
+    ["title", 1],
+    () => getTitles(1)
+  );
+  const { data: HeroSection, isLoading: isHeroLoading } = useQuery(
+    "hero",
+    getHeroSection
+  );
   const { data: navbarItems } = useNavbarItemsId(2);
-  const {handleScroll} = useHandleScroll();
+  const { handleScroll } = useHandleScroll();
+
+  if (isTitleLoading || isHeroLoading) return <div>Loading...</div>;
 
   const heroDataArray = HeroSection ? HeroSection[0] : null;
   if (!TitleSection || !heroDataArray) return null;
-
 
   return (
     <div className="relative w-full h-screen">
       <img
         className="w-full h-full object-cover object-center"
-        src={heroDataArray?.heroImage_Url}
+        src={`${heroDataArray?.heroImage_Url}.avif`} 
         alt="Hero Image"
-        loading='lazy'
+        loading="lazy"
       />
       <div className="absolute inset-0 bg-black bg-opacity-30 flex flex-col items-center justify-center">
         <div className="text-center text-white text-5xl font-bold font-'Poppins' mb-6">
-          <p>{heroDataArray?.hero_Title || "Hogar de Ancianos nuestra señora del Rosario"}</p>
+          <p>
+            {heroDataArray?.hero_Title ||
+              "Hogar de Ancianos Nuestra Señora del Rosario"}
+          </p>
         </div>
         <div className="text-center text-white text-2xl font-medium font-'Poppins' mb-12">
           <p>{heroDataArray?.subtitle_Hero}</p>
         </div>
         <a href={navbarItems?.urlNav}>
-  <button
-    onClick={(event: any) => {
-      handleScroll(event, navbarItems?.urlNav);
-    }}
-    className="bg-gray-100 bg-opacity-20 text-white text-[16px] sm:text-[20px] font-bold font-'Poppins' py-2 sm:py-3 px-4 sm:px-6 rounded-[10px] sm:rounded-[15px] shadow transition-all duration-300 hover:bg-white hover:text-[#0d313f]"
-  >
-    {TitleSection?.title_Text_Section || "Sobre Nosotros"}
-  </button>
-</a>
+          <button
+            onClick={(event: any) => {
+              handleScroll(event, navbarItems?.urlNav);
+            }}
+            className="bg-gray-100 bg-opacity-20 text-white text-[16px] sm:text-[20px] font-bold font-'Poppins' py-2 sm:py-3 px-4 sm:px-6 rounded-[10px] sm:rounded-[15px] shadow transition-all duration-300 hover:bg-white hover:text-[#0d313f]"
+          >
+            {TitleSection?.title_Text_Section || "Sobre Nosotros"}
+          </button>
+        </a>
       </div>
     </div>
   );
