6f004108da4961b51ea0d95d49f4624e54b6a18f
TosTar26 <ozsanchez2004@gmail.com>
2025-02-10 17:41:52 -0600

    refactor: new funcionalities in applicationform

--- nuestra_senora_informativa/src/Services/ServiceInformative.ts
+++ nuestra_senora_informativa/src/Services/ServiceInformative.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AplicationFormDTO ,  FormDonationCreateDto, FormVolunteerCreateDto } from '../Types/informativeType';
 
 
-const URLBase =  'https://nuestra-senora-del-rosario-backend-2.onrender.com/api';
+const URLBase =  'https://localhost:7066/api';
 
 export const getAboutUsSection = async () => {
   const response = await axios.get(`${URLBase}/AboutUsSection`);
--- nuestra_senora_informativa/src/Types/informativeType.ts
+++ nuestra_senora_informativa/src/Types/informativeType.ts
@@ -120,20 +120,18 @@ VoluntarieTypeId: number;
 
 export type AplicationFormDTO = {
   id_ApplicationForm: number;
-  id_Applicant: number;
-  name_AP: string;
-  lastname1_AP: string;
-  lastname2_AP: string;
-  age_AP: number;
-  cedula_AP: string;
-  location: string;
-  id_Guardian: number;
-  name_GD: string;
-  lastname1_GD: string;
-  lastname2_GD: string;
-  cedula_GD: string;
-  phone_GD: string;
-  email_GD: string;
   applicationDate: string;
+  guardianName: string;
+  guardianLastName1: string;
+  guardianLastName2: string;
+  guardianCedula: string;
+  guardianEmail: string;
+  guardianPhone: string;
+  name_AP: string;
+  lastName1_AP: string;
+  lastName2_AP: string;
+  cedula_AP: string;
+  age_AP: number;
+  location_AP: string;
   status_Name: string;
-}
+};
