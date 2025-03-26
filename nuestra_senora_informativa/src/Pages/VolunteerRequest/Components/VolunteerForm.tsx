1b72f8c8911b6ca5ed097450bb6842d2d665cc49
FabiolaC1 <fabicarrera1904@gmail.com>
2025-03-25 19:28:25 -0600

    Implementacion de confirmationModal a VolunteerForm

--- nuestra_senora_informativa/src/Pages/VolunteerRequest/Components/VolunteerForm.tsx
+++ nuestra_senora_informativa/src/Pages/VolunteerRequest/Components/VolunteerForm.tsx
@@ -5,17 +5,23 @@ import { useSiteSettings } from '../../../Hooks/useSiteSettings';
 import { useModal } from '../../../Hooks/useModal';
 import { useVoluntarieType } from '../Hooks/useVoluntarieType';
 import { usePostFormVolunteer } from '../Hooks/usePostVolunteerFrm';
-import {InputForm, CustomSelect, ConfirmationModal, RateLimitModal, LoadingSpinner} from '../../../Components';
+import {
+  InputForm,
+  CustomSelect,
+  ConfirmationModal,
+  RateLimitModal,
+  LoadingSpinner,
+} from '../../../Components';
 
 const VolunteerForm = () => {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm<FormVolunteerCreateDto>();
+  const { register, handleSubmit, formState: { errors }, reset, watch } = useForm<FormVolunteerCreateDto>();
   const { data: siteSettings } = useSiteSettings();
   const siteSettingsData = siteSettings ? siteSettings[0] : null;
   const { data: voluntarieTypes, isLoading, isError } = useVoluntarieType();
   const { isOpen, openModal, closeModal } = useModal();
   const navigate = useNavigate();
 
-  const {mutation, setRateLimitExceeded, rateLimitExceeded} = usePostFormVolunteer();
+  const { mutation, setRateLimitExceeded, rateLimitExceeded } = usePostFormVolunteer();
 
   const onSubmit = (data: FormVolunteerCreateDto) => {
     mutation.mutate(data, {
@@ -26,11 +32,12 @@ const VolunteerForm = () => {
           closeModal();
           navigate('/');
         }, 4000);
-      }
+      },
     });
   };
 
   const today = new Date().toISOString().split('T')[0];
+  const startDate = watch('Delivery_Date');
 
   if (isLoading) return <LoadingSpinner />;
   if (isError) return <div>Error al cargar los tipos de voluntariado</div>;
@@ -42,7 +49,11 @@ const VolunteerForm = () => {
       </h2>
       <div className="flex items-center justify-center my-6 w-full max-w-lg">
         <div className="w-1/4 sm:w-1/3 md:w-1/2 border-t-2 border-[#0d313f]"></div>
-        <img className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] mx-4" src={siteSettingsData?.icon_HGA_Url} alt="Logo de la institución" />
+        <img
+          className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] mx-4"
+          src={siteSettingsData?.icon_HGA_Url}
+          alt="Logo de la institución"
+        />
         <div className="w-1/4 sm:w-1/3 md:w-1/2 border-t-2 border-[#0d313f]"></div>
       </div>
 
@@ -55,14 +66,14 @@ const VolunteerForm = () => {
           <InputForm
             label="Nombre"
             id="nombre"
-            placeholder='Nombre del voluntario'
+            placeholder="Nombre del voluntario"
             error={errors.Vn_Name?.message}
             {...register('Vn_Name', { required: 'El nombre es obligatorio' })}
           />
           <InputForm
             label="Primer Apellido"
             id="primerApellido"
-            placeholder='Primer apellido del voluntario'
+            placeholder="Primer apellido del voluntario"
             error={errors.Vn_Lastname1?.message}
             {...register('Vn_Lastname1', { required: 'El primer apellido es obligatorio' })}
           />
@@ -72,14 +83,14 @@ const VolunteerForm = () => {
           <InputForm
             label="Segundo Apellido"
             id="segundoApellido"
-            placeholder='Segundo apellido del voluntario'
+            placeholder="Segundo apellido del voluntario"
             error={errors.Vn_Lastname2?.message}
             {...register('Vn_Lastname2', { required: 'El segundo apellido es obligatorio' })}
           />
           <InputForm
             label="Cédula"
             id="cedula"
-            placeholder='Ejem: 102340567'
+            placeholder="Ejem: 102340567"
             error={errors.Vn_Cedula?.message}
             {...register('Vn_Cedula', {
               required: 'La cédula es obligatoria',
@@ -99,7 +110,7 @@ const VolunteerForm = () => {
           <InputForm
             label="Correo Electrónico"
             id="correo"
-            placeholder='Ejemplo: correo@dominio.com'
+            placeholder="Ejemplo: correo@dominio.com"
             type="email"
             error={errors.Vn_Email?.message}
             {...register('Vn_Email', {
@@ -113,7 +124,7 @@ const VolunteerForm = () => {
           <InputForm
             label="Teléfono"
             id="telefono"
-            placeholder='Ejemplo: 88888888'
+            placeholder="Ejemplo: 88888888"
             type="tel"
             error={errors.Vn_Phone?.message}
             {...register('Vn_Phone', { required: 'El teléfono es obligatorio' })}
@@ -121,23 +132,25 @@ const VolunteerForm = () => {
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-        <InputForm
+          <InputForm
             label="Fecha de Inicio"
             id="fechaInicio"
             type="date"
-            min={today} // 🔹 Bloquea fechas pasadas
+            min={today}
             error={errors.Delivery_Date?.message}
-            className={`${
-              errors.Delivery_Date ? 'border-red-500 bg-red-100' : ''
-            }`}
-            {...register('Delivery_Date', { required: 'La fecha de inicio es obligatoria' })}
+            {...register('Delivery_Date', {
+              required: 'La fecha de inicio es obligatoria',
+            })}
           />
           <InputForm
             label="Fecha de Fin"
             id="fechaFin"
             type="date"
+            min={startDate ? new Date(startDate).toISOString().split('T')[0] : undefined}
             error={errors.End_Date?.message}
-            {...register('End_Date', { required: 'La fecha de fin es obligatoria' })}
+            {...register('End_Date', {
+              required: 'La fecha de fin es obligatoria',
+            })}
           />
         </div>
 
@@ -172,7 +185,11 @@ const VolunteerForm = () => {
           </Link>
         </div>
       </form>
+
+      {/* Modal de confirmación */}
       <ConfirmationModal isOpen={isOpen} onClose={closeModal} />
+
+      {/* Modal de límite de solicitudes */}
       <RateLimitModal isOpen={rateLimitExceeded} onClose={() => setRateLimitExceeded(false)} />
     </div>
   );
